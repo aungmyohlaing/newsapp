00783e31b150e25f7260dbfa29b8c1e7ddb04094
aung <amhlaing@gmail.com>
2020-06-21 02:56:19 +0700

    Updates

---
+++ precache-manifest.5746a0ee143eb5cfab2c7a1f29e63a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51034e7b30c4e76931319a3562ff488",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "8bad9ae7b33e1e0a40b5",
+    "url": "/newsapp/static/css/main.6a81d272.chunk.css"
+  },
+  {
+    "revision": "14fce72ccc273b34c67b",
+    "url": "/newsapp/static/js/2.57e39a88.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/newsapp/static/js/2.57e39a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bad9ae7b33e1e0a40b5",
+    "url": "/newsapp/static/js/main.8f0410f9.chunk.js"
+  },
+  {
+    "revision": "81b8192a84107fbf2e30",
+    "url": "/newsapp/static/js/runtime-main.0d8c95c5.js"
+  }
+]);
